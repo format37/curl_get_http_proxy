fce8b804ab638df5c0ebcba6cfb4512d641d8f4a
Alex <format37@gmail.com>
2024-01-26 11:31:46 +0400

    modified:   Dockerfile 	modified:   build.sh

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY requirements.txt /server
 RUN pip3 install -r requirements.txt --no-cache-dir
 COPY server.py /server
 COPY config.ini /server
-CMD ["uvicorn", "server:server", "--host", "0.0.0.0", "--port", "5000"]
+CMD ["uvicorn", "server:app", "--host", "0.0.0.0", "--port", "5000"]
--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 # sudo docker build -t telegram_proxy .
 
 # Build with name telegram_proxy
-sudo docker build -t --force-rm telegram_proxy .
+# sudo docker build -t --force-rm telegram_proxy .
 # Build with name telegram_proxy force rebuild and recreate
-# sudo docker build --no-cache --force-rm -t telegram_proxy .
+sudo docker build --no-cache --force-rm -t telegram_proxy .
 # Run as a daemon
 # sudo docker run -d telegram_proxy
 # Run as a daemon with 5000 port
