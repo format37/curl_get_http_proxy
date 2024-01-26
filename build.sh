d0a39c504efde1b40e8df1d0c08566d6e403ca42
Alex <format37@gmail.com>
2024-01-26 11:20:57 +0400

    new file:   Dockerfile 	new file:   build.sh 	new file:   requirements.txt 	modified:   server.py

---
+++ build.sh
@@ -0,0 +1,11 @@
+# Build with name telegram_proxy
+# sudo docker build -t telegram_proxy .
+
+# Build with name telegram_proxy
+sudo docker build -t --force-rm telegram_proxy .
+# Build with name telegram_proxy force rebuild and recreate
+# sudo docker build --no-cache --force-rm -t telegram_proxy .
+# Run as a daemon
+# sudo docker run -d telegram_proxy
+# Run as a daemon with 5000 port
+sudo docker run -d -p 5000:5000 telegram_proxy
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-from flask import Flask, request
+from fastapi import FastAPI, HTTPException
 import requests
 import configparser
+from pydantic import BaseModel
 
-app = Flask(__name__)
+app = FastAPI()
 
 # Load configuration
 config = configparser.ConfigParser()
@@ -11,15 +12,22 @@ bot_token = config['telegram']['bot_token']
 chat_id = config['telegram']['chat_id']
 telegram_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
 
-@app.route('/send_message', methods=['GET'])
-def send_message():
-    message = request.args.get('message', 'No message provided')
+class Message(BaseModel):
+    message: str
+
+@app.get("/send_message")
+def send_message(message: str = 'No message provided'):
     payload = {
         'chat_id': chat_id,
         'text': message
     }
     response = requests.post(telegram_url, data=payload)
-    return response.text
+    
+    if response.status_code != 200:
+        raise HTTPException(status_code=400, detail=f"Error: {response.text}")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    return {"detail": "Message sent successfully"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
